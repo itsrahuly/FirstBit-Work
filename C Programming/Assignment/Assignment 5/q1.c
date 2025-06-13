49e4757466c99ca8db6cc1baf63b1ac000948ade
itsrahuly <rahulyelleboinwad@gmail.com>
2025-06-13 16:26:35 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 5/q1.c
@@ -0,0 +1,9 @@
+void main ()
+{
+    int n = 4; 
+    for (int i = 0; i < n; i++) 
+	{         
+        for (int j = 0; j < n; j++)      
+            printf("*  ");                     
+        printf("\n");                    
+    }}
