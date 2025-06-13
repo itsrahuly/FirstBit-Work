5a0775ca562ea797f5f561df08ce58caa98df2f5
itsrahuly <rahulyelleboinwad@gmail.com>
2025-06-13 15:42:28 +0530

    Add files via upload

---
+++ C Programming/Assignment/Assignment 4/q2.c
@@ -0,0 +1,22 @@
+void  main() 
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers between 1 and %d are:\n", n);
+
+    for (int i = 1; i <= n; i++) 
+	{
+        int isPrime = 1;
+
+        for (int j = 2; j <= i / 2; j++) 
+            if (i % j == 0) 
+			{
+                isPrime = 0;
+                break;
+            }
+        if (isPrime)
+            printf("%d ", i);   
+    }
+}
