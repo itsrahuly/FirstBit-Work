9aa6ef280ba55f6c4b37271e8cf5a024037cdf72
Rahul Yelleboinwad <rahulyelleboinwad@gmail.com>
2025-08-06 19:55:04 +0530

    complete

---
+++ MYSQL/Assignment 1/q1.sql
@@ -0,0 +1,42 @@
+1. Customers
+CustomerID (Primary Key)
+Email
+
+2. Rooms
+RoomID (Primary Key)
+CustomerID (Foreign Key)
+RoomType
+
+3. RoomTypes
+RoomType (Primary Key)
+Rent
+
+4. Services
+ServiceID (Primary Key)
+CustomerID (Foreign Key)
+ServiceType
+
+5. FoodOrders
+OrderID (Primary Key)
+CustomerID (Foreign Key)
+FoodItem
+
+6. Laundry
+LaundryID (Primary Key)
+CustomerID (Foreign Key)
+Quantity
+RatePerCloth
+
+7. SiteVisits
+VisitID (Primary Key)
+
+CustomerID (Foreign Key)
+
+VehicleType
+
+RatePerDay
+
+8. Invoices
+InvoiceID (Primary Key)
+
+CustomerID (Foreign Key)
